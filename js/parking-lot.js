a2b89368ddb8c16b7542b7607ce4ab7ac92e995e
Lexi <Michelcalex+github@gmail.com>
2016-12-26 12:51:48 -0500

    Fixed code to show what cars are parked in the parking lot. Made lot container wider to fix context better. Added getNewInfo function to update the car lot after clicking the button.

--- js/parking-lot.js
+++ js/parking-lot.js
@@ -59,7 +59,7 @@ function getCarLots() {
         //console.log(response);
 
         let lotList = document.querySelector('#lot-info'); //this is my 'ul' from my html 
-
+         lotList.textContent = '';
         for (let i = 0; i < response.length; i++) {
             let lot = document.createElement('li'); //this creates a new 'li' element for my car lots everytime it runs through the loop.
 
@@ -70,7 +70,8 @@ function getCarLots() {
                 document.querySelector('#car-lot-template').innerHTML, 
                 { id: response[i].id,        //response[i] is the information coming from the GET request
                   lotCapacity: response[i].capacity,
-                  lotCost: response[i].cost
+                  lotCost: response[i].cost,
+                  carsParked: response[i].cars,
                 }
             )
 
@@ -135,13 +136,9 @@ function showCars() {
             });
 
             let lot2Button = newCar.querySelector('#lot-2');
-            lot1Button.addEventListener('click', function () {
+            lot2Button.addEventListener('click', function () {
                 updateCars(2, cars[i]);
             });
-
-        // TODO: add third
-
-        // POST: lot id, the car
         
         carList.appendChild(newCar);    //im appending my li, newCar to my parent element which in this case is carList
     }         
@@ -151,7 +148,7 @@ function showCars() {
 
 
 function updateCars(lotId, car) { //here is where my updateCars function exist. I am passing my two parameters from above, so the id of the car and cars[i]. EX) updateCars(2, cars[i])
-
+    console.log('here');
     let newCarParked = {     //I am creating a new variable called newCarParked which is sending this info to my backend with the parameters THAT they are ACCEPTING.  
         id: lotId,   // lotId here is referring to the same numbers from above in the updateCars. EX) updateCars(1, cars[i])
         make: car.make,  //this is coming from what every car at the time is equal too and its make which came from here {carMake: cars[i].make,} when I was rendering newCar.innerHTML
@@ -163,162 +160,15 @@ function updateCars(lotId, car) { //here is where my updateCars function exist. 
 
     let request = new XMLHttpRequest();
     request.open('POST', 'https://warm-waters-57933.herokuapp.com/update');
-    // request.addEventListener('load', function () {
-    //     window.setInterval(getCarLots, 1000);
-    // });
+    request.addEventListener('load', function () {
+        getNewInfo(); 
+    });
 
     request.send(JSON.stringify(newCarParked));   //this is stringifying newCarParked for the backend. 
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let cars = [
-//     {
-//         make: 'Ford',
-//         model: 'Mustang',
-//         size: 5,
-//         money: 30,
-//     },
-//     {
-//         make: 'Subaru',
-//         model: 'Forester',
-//         size: 7,
-//         money: 70,
-//     },
-//     {
-//         make: 'Smart',
-//         model: 'Passion Cabrio',
-//         size: 1,
-//         money: 15,
-//     }
-// ]
-
-// let lots = [ //once you get data from backend-delete this array and set it to response****
-//     {
-//         id: 0,
-//         capacity: 15,
-//         rate: 5,
-//     },
-//     {
-//         id: 1,
-//         capacity: 15,
-//         rate: 5,
-//     },
-//     {
-//         id: 2,
-//         capacity: 15,
-//         rate: 5,
-//     }
-// ]
-
-
-
-
-// //when the page loads, request list of lots
-// window.addEventListener('load', function() {
-//     getCarLots();
-
-//     //we didn't have to get it from an AJAX request this time. Before we were getting it from AJAX. 
-//     for(let i = 0; i < cars.length; i++) {
-//             showCars(cars[i]);
-//         }
-// });
-
-
-// // function parkCar () {
-   
-
-
-// // }
-
-// //get car lots
-// function getCarLots() {
-//     let request = new XMLHttpRequest();
-//     request.open('GET', 'http://#/lots');
-//     request.addEventListener('load', function() {
-//         console.log('Weve got cars weeeeeeee');
-//         let response = JSON.parse(request.responseText);
-//         console.log(response);
-
-//         //after you got the lots, build the top out with the Dom just like you did for the cars******
-//     });
-
-//     request.send();
-// }
-
-
-// //Show car
-// function showCars(car) {
-
-//     let carList = document.querySelector('ul');
-
-//         //append this to ul 
-//         let newCar = document.createElement('li');
-//         carList.appendChild(newCar);
-
-//             //create make of the car
-//             let makeCar = document.createElement('p');
-//             makeCar.textContent = car.make;
-//             newCar.appendChild(makeCar);
-
-//             //create model of the car
-//             let modelCar = document.createElement('p');
-//             modelCar.textContent = car.model;
-//             newCar.appendChild(modelCar);
-
-//             //create size of the car
-//             let sizeCar = document.createElement('p');
-//             sizeCar.textContent = car.size;
-//             newCar.appendChild(sizeCar);
-
-//             //create money of the car
-//             let moneyCar = document.createElement('p');
-//             moneyCar.textContent = car.money;
-//             newCar.appendChild(moneyCar);
-
-//             // create button
-//             // when clicked, send the current car
-//                 for (let i =0; i < lots.length; i++) {
-//                     let lotBtn = document.createElement('button');
-//                     lotBtn.textContent = "Add Lot " + lots[i].id;
-//                     newCar.appendChild(lotBtn);
-//                     lotBtn.addEventListener('click', function() {
-//                         updateCarLot(car, lots[i]);
-//                     });
-//                 }              
-            
-// }
-
-// function updateCarLot(car, lot) {
-//     let params = {
-//         id: lot.id,
-//         make: car.make,
-//         model: car.model,
-//         money: car.money,
-//         size: car.size
-//     };
-
-//     let request = new XMLHttpRequest();
-//     request.open('POST', 'http://#/update');
-//     request.addEventListener('load', function() {
-//         console.log('look! it updated!');
-//     });
-
-//     request.send(JSON.stringify(params));
-// }   
+ 
+function getNewInfo(){
+    
+    getCarLots();
+}
